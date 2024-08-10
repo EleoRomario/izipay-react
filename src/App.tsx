388fee5e8291064ddc162f1ce48eb7cf2f0cf296
eleo romario <eleo.romario@gmail.com>
2024-08-10 12:29:12 -0500

    chore: Update npm dependencies to include @paypal/react-paypal-js 8.5.0

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,20 @@
+import { PayPalButtons, PayPalScriptProvider } from "@paypal/react-paypal-js";
 import PaymentForm from "./components/PaymentForm";
 
 function App() {
+	const initialOptions = {
+		clientId: "test",
+		currency: "USD",
+		intent: "capture",
+	};
 	return (
 		<>
 			<h1>Payment Integration</h1>
 			<PaymentForm />
+			<PayPalScriptProvider options={initialOptions}>
+				<h1>PayPal</h1>
+				<PayPalButtons style={{ layout: "horizontal" }} />
+			</PayPalScriptProvider>
 		</>
 	);
 }
